d3bb4ed9f84ff55b9763cc88182f8b5a9d8035b2
Anna <annajaninaschmidt@gmail.com>
2023-01-05 14:46:08 +1100

    html navbar set up

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// API key
+var apiKey = '84451525b0d6082d8cc3aa7f5d04ae49';
+
+
+
+
+// function for weather now
+function weatherNow(city) {
+    var weatherNowURL = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+}
+
+
+
+timezone
+current
+current.humidity
+current.dt
+current.wind_speed
+current.weather.icon
