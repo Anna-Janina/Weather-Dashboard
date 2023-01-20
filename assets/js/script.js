9635f33e70c6d8f8a95642cbb196e7927b272973
Anna <annajaninaschmidt@gmail.com>
2023-01-20 21:34:31 +1100

    cleaned up js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,8 @@ function weatherNow(city) {
     .then(res => res.json())
     .then(data => {
         updateWeatherInfo(data);
-        displayForecast(city);
+        // getWeatherData(city);
+        // displayForecast(city);
         saveSearchToLocalStorage(city)
     })
     .catch(error => {
@@ -34,12 +35,51 @@ function weatherNow(city) {
 }
 
 
-function getWeatherData(city) {
-    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-      .then(response => response.json());
-  }
 
-  function getWeatherForecast() {
+function displayWeather(event) {
+    event.preventDefault();
+    if (searchCity.val().trim() !== "") {
+        city = searchCity.val().trim();
+            weatherNow(city);
+            getWeatherForecast(city)
+            // getWeatherData(city)
+            .then(updateWeatherInfo)
+            .then(() => saveSearchToLocalStorage(city))
+            // .then(() => displayForecast(city))
+            .catch(error => {
+              $("#error-message").html("Error: " + error.message);
+              $("#error-message").show();
+            });
+        }
+    }
+
+
+function convertTemperature(temp, unit) {
+    if (unit === "C") {
+        return temp - 273.15;
+        } else if (unit === "F") {
+          return (temp - 273.15) * 9/5 + 32;
+        }
+    }
+
+function updateWeatherInfo(data) {
+    let iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+    var date=new Date(data.dt*1000).toLocaleDateString();
+    $("#current-city").html(data.name + " " + "("+date+")");
+    $("#current-city").append("<img src='" + iconUrl + "'>");
+    let today = new Date();
+    let day = today.toLocaleDateString();
+    let temperatureInCelsius = data.main.temp - 273.15;
+    $("#temperature").html(" " + temperatureInCelsius + "°C");
+    let windSpeed = data.wind.speed;
+    $("#wind-speed").html(" " + windSpeed + " m/s");
+    let humidity = data.main.humidity;
+    $("#humidity").html(" " + humidity + " %");
+}
+
+
+
+function getWeatherForecast(city) {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
     .then(res => res.json())
     .then(data => {
@@ -67,131 +107,6 @@ function getWeatherData(city) {
     .catch(err => console.log(err))
 }
 getWeatherForecast()
-/////////////////////////////////////////
-function updateWeatherInfo(data) {
-    let iconUrl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
-    var date=new Date(data.dt*1000).toLocaleDateString();
-    $("#current-city").html(data.name + " " + "("+date+")");
-    $("#current-city").append("<img src='" + iconUrl + "'>");
-    let today = new Date();
-    let day = today.toLocaleDateString();
-    let temperatureInCelsius = data.main.temp - 273.15;
-    $("#temperature").html(" " + temperatureInCelsius + "°C");
-    let windSpeed = data.wind.speed;
-    $("#wind-speed").html(" " + windSpeed + " m/s");
-    let humidity = data.main.humidity;
-    $("#humidity").html(" " + humidity + " %");
-}
-
-function weatherNow(city) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey)
-    .then(res => res.json())
-    .then(data => {
-        updateWeatherInfo(data);
-        saveSearchToLocalStorage(city)
-    })
-    .catch(error => {
-        $("#error-message").html("Error: " + error.message);
-        $("#error-message").show();
-    })
-}
-
-
-function convertTemperature(temp, unit) {
-    if (unit === "C") {
-      return temp - 273.15;
-    } else if (unit === "F") {
-      return (temp - 273.15) * 9/5 + 32;
-    }
-  }
- 
-  
-function getForecastData(city) {
-    return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
-      .then(response => response.json());
-}
-
-function displayForecast(city) {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey)
-        .then(res => res.json())
-        .then(data => {
-            updateForecastInfo(data);
-        })
-        .catch(error => {
-            $("#error-message").html("Error: " + error.message);
-            $("#error-message").show();
-        });
-}
-
-function displayForecast(event) {
-    event.preventDefault();
-    if (searchCity.val().trim() !== "") {
-        city = searchCity.val().trim();
-        getForecastData(city)
-            .then(updateForecastInfo)
-            .catch(error => {
-                $("#error-message").html("Error: " + error.message);
-                $("#error-message").show();
-            });
-    }
-}
-
-function getForecastData(city) {
-    return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
-        .then(response => response.json());
-}
-
-
-
-function updateForecastInfo(data) {
-    let forecastData = data.list;
-
-    for(let i = 0; i < forecastData.length; i++) {
-        // Get the date and time of the forecast
-        let forecastTime = forecastData[i].dt_txt;
-        let forecastDate = new Date(forecastTime);
-
-        // Only display forecasts for 15:00 (3:00 PM)
-        if(forecastDate.getUTCHours() === 15) {
-            let forecastIndex = i;
-            let fDate = forecastData[forecastIndex].dt_txt;
-            let fImg = "http://openweathermap.org/img/w/" + forecastData[forecastIndex].weather[0].icon + ".png";
-            let fTemp = forecastData[forecastIndex].main.temp - 273.15;
-            let fWind = forecastData[forecastIndex].wind.speed;
-            let fHumidity = forecastData[forecastIndex].main.humidity;
-
-            // Display the forecast information
-            $("#fDate" + i).html(fDate);
-            $("#fImg" + i).html("<img src='" + fImg + "'>");
-            $("#fTemp" + i).html(" " + fTemp + "°C");
-            $("#fWind" + i).html(" " + fWind + " m/s");
-            $("#fHumidity" + i).html(" " + fHumidity + " %");
-        }
-    }
-}
-
-
-
-
-
-function displayWeather(event) {
-    event.preventDefault();
-    if (searchCity.val().trim() !== "") {
-      city = searchCity.val().trim();
-      getWeatherData(city)
-        .then(updateWeatherInfo)
-        .then(() => saveSearchToLocalStorage(city))
-        .then(() => displayForecast(city))
-        .catch(error => {
-          $("#error-message").html("Error: " + error.message);
-          $("#error-message").show();
-        });
-    }
-  }
-
-
-
-
 
 
 
@@ -211,14 +126,4 @@ $("#clear-history").on("click", function (event) {
     localStorage.clear();
   })
 
-console.log(localStorage) 
-
-
-// "icon": "10d"
-// {"coord":{"lon":10.99,"lat":44.34},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],"base":"stations","main":{"temp":275.49,"feels_like":275.49,"temp_min":271.56,"temp_max":278.19,"pressure":1023,"humidity":87,"sea_level":1023,"grnd_level":933},"visibility":10000,"wind":{"speed":0.4,"deg":262,"gust":0.66},"clouds":{"all":55},"dt":1673669458,"sys":{"type":2,"id":2004688,"country":"IT","sunrise":1673678954,"sunset":1673712012},"timezone":3600,"id":3163858,"name":"Zocca","cod":200}
-
-
-
-
-
-
+console.log(localStorage)  
